c5089bab81f119630216279d86207a2ac985e82d
erolcetinok <erol.cetinok@gmail.com>
2025-12-22 19:44:27 -0800

    edits to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: 40, fontFamily: "system-ui" }}>
       <h1>Erol Cetinok</h1>
-      <p>Robotics • Mechanical Engineering • Research</p>
+      <p>My Portfolio</p>
     </main>
   );
 }
