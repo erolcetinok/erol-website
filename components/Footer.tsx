baab3f4da0e4612797659d9ea170d231d5879533
erolcetinok <erol.cetinok@gmail.com>
2025-12-22 22:44:55 -0800

    update to logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,70 +1,58 @@
 import Image from "next/image";
 
-// This runs once per page render and gives the current year automatically.
-// Nice because you never have to update "© 2026" manually.
 const year = new Date().getFullYear();
 
 export default function Footer() {
   return (
-    // Outer footer container (styled by .site-footer)
     <footer className="site-footer">
-      {/* Inner container: max width + flex layout */}
-      <div className="site-footer__inner">
-        {/* Left: brand mini version */}
-        <div className="footer-brand">
-          <Image
-            src="/logo.png"
-            alt="Erol Cetinok logo"
-            width={24}
-            height={24}
-          />
+      <div className="site-footer__inner site-footer__inner--stacked">
+        {/* Brand (scaled down vs header) */}
+        <div className="footer-brand footer-brand--stacked">
+          <Image src="/logo.png" alt="Erol Cetinok logo" width={42} height={42} />
           <span className="footer-brand__name">Erol Cetinok</span>
         </div>
 
-        {/* Middle: social links */}
-        <div className="footer-links">
-          {/* Each link opens in a new tab:
-              - target="_blank" opens new tab
-              - rel="noreferrer" is a security/privacy best practice */}
+        {/* Social icons */}
+        <div className="footer-links footer-links--centered">
           <a
-            href="https://www.linkedin.com/in/YOUR_LINKEDIN"
+            href="https://www.linkedin.com/in/erol-cetinok-387830348/"
             target="_blank"
             rel="noreferrer"
             aria-label="LinkedIn"
             className="icon-link"
           >
-            {/* Decorative icons use empty alt text to avoid screen reader noise */}
-            <Image src="/icons/linkedin.svg" alt="" width={20} height={20} />
+            <Image src="/icons/linkedin.svg" alt="" width={26} height={26} />
           </a>
 
           <a
-            href="https://github.com/YOUR_GITHUB"
+            href="https://github.com/erolcetinok"
             target="_blank"
             rel="noreferrer"
             aria-label="GitHub"
             className="icon-link"
           >
-            <Image src="/icons/github.svg" alt="" width={20} height={20} />
+            <Image src="/icons/github.svg" alt="" width={26} height={26} />
           </a>
 
           <a
-            href="https://www.youtube.com/@YOUR_YOUTUBE"
+            href="https://www.youtube.com/@ErolBuilds"
             target="_blank"
             rel="noreferrer"
             aria-label="YouTube"
             className="icon-link"
           >
-            <Image src="/icons/youtube.svg" alt="" width={20} height={20} />
+            <Image src="/icons/youtube.svg" alt="" width={26} height={26} />
           </a>
         </div>
 
-        {/* Right: copyright + email */}
-        <div className="footer-meta">
-          <div>© {year} Erol Cetinok. All rights reserved.</div>
+        {/* Text lines */}
+        <div className="footer-lines">
+          <div className="footer-line footer-line--shout">
+            Erol Cetinok © {year}
+          </div>
 
-          {/* mailto: opens the user's email client addressed to you */}
-          <a className="email-link" href="mailto:YOUR_EMAIL">
-            YOUR_EMAIL
+          <a className="footer-line footer-line--shout footer-email" href="mailto:erol.cetinok@gmail.com">
+            CONTACT @ erol (at) gmail (dot) com
           </a>
         </div>
       </div>
