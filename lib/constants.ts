3dd98e59394d94182ed48b4180e687c552475065
erolcetinok <erol.cetinok@gmail.com>
2026-01-22 14:37:34 -0800

    Got rid of blogs page

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,7 @@ export const NAV_ITEMS = [
   { href: "/", label: "Home" },
   { href: "/about", label: "About" },
   { href: "/projects", label: "Projects" },
-  { href: "/blog", label: "Blog" },
+  // { href: "/blog", label: "Blog" },
   { href: "/contact", label: "Contact" },
 ] as const;
 
